ee981c00877ad3da1278fc600f0c0636be569b6f
Igor <GI_Something@protonmail.com>
2020-03-26 16:23:25 +0200

    feat: Create simple game
    Ask user to input a word.
    If random word(4 words below) == inputted word output "You survived!" else "You are hanged!".
    Words: "python" ,"java", "kotlin", "javascript".

--- src/hangman/hangman.py
+++ src/hangman/hangman.py
@@ -1,2 +1,24 @@
-print("H A N G M A N")
-print("The game will be available soon.")
+from random import choice
+
+
+def input_():
+    return input("Guess the word: ").strip().lower()
+
+
+def output(data_):
+    print(data_)
+
+
+def get_outcome(program_word_, user_word_):
+    return "You survived!" if program_word_ == user_word_ else "You are hanged!"
+
+
+output("H A N G M A N")
+
+WORDS = ("java", "kotlin", "python", "javascript")
+
+rand_word = choice(WORDS)
+
+user_word = input_()
+
+output(get_outcome(rand_word, user_word))
