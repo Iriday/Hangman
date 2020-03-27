8c96fd8367a550ed29fbdd47ad1846ec033db576
Igor <GI_Something@protonmail.com>
2020-03-27 21:32:13 +0200

    feat: Output hint. Increase num of words to 20

--- src/hangman/hangman.py
+++ src/hangman/hangman.py
@@ -1,8 +1,8 @@
 from random import choice
 
 
-def input_():
-    return input("Guess the word: ").strip().lower()
+def input_(message=""):
+    return input(message).strip().lower()
 
 
 def output(data_):
@@ -13,12 +13,18 @@ def get_outcome(program_word_, user_word_):
     return "You survived!" if program_word_ == user_word_ else "You are hanged!"
 
 
+def replace_trailing(string, num, char=""):
+    return string[0:len(string) - num:1].ljust(len(string), char)
+
+
 output("H A N G M A N")
 
-WORDS = ("java", "kotlin", "python", "javascript")
-
+WORDS = ("java", "kotlin", "python", "javascript", "rust", "ruby", "perl", "swift", "scratch", "haskel", "pascal",
+         "fortran", "ada", "apex", "groovy", "dart", "scala", "c++", "assembly", "prolog")
 rand_word = choice(WORDS)
 
-user_word = input_()
+word_hint = replace_trailing(rand_word, len(rand_word) - 1, "-")
+
+user_word = input_(f"Guess the word {word_hint}: ")
 
 output(get_outcome(rand_word, user_word))
