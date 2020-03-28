744852dda395ff8c9841bc9f4b74d794f2451c16
Igor <GI_Something@protonmail.com>
2020-03-28 19:10:43 +0200

    feat: Input(guess) the letter of the word instead of the entire word

--- src/hangman/hangman.py
+++ src/hangman/hangman.py
@@ -1,30 +1,60 @@
 from random import choice
+import re
 
 
 def input_(message=""):
     return input(message).strip().lower()
 
 
-def output(data_):
+def output(data_=""):
     print(data_)
 
 
-def get_outcome(program_word_, user_word_):
-    return "You survived!" if program_word_ == user_word_ else "You are hanged!"
+def create_list_of(size, char=" "):
+    return list(char * size)
 
 
-def replace_trailing(string, num, char=""):
-    return string[0:len(string) - num:1].ljust(len(string), char)
+def remove_all(string, regex):
+    return re.sub(regex, "", string)
 
 
-output("H A N G M A N")
+def open_letter(entire_word, word_to_update, letter):
+    opened = False
+    for i in range(0, len(entire_word)):
+        if entire_word[i] == letter:
+            word_to_update[i] = letter
+            opened = True
+    return opened
 
-WORDS = ("java", "kotlin", "python", "javascript", "rust", "ruby", "perl", "swift", "scratch", "haskel", "pascal",
-         "fortran", "ada", "apex", "groovy", "dart", "scala", "c++", "assembly", "prolog")
-rand_word = choice(WORDS)
 
-word_hint = replace_trailing(rand_word, len(rand_word) - 1, "-")
+def start_game():
+    output("H A N G M A N")
 
-user_word = input_(f"Guess the word {word_hint}: ")
+    WORDS = ("java", "kotlin", "python", "javascript", "rust", "ruby", "perl", "swift", "scratch", "haskel", "pascal",
+             "fortran", "ada", "apex", "groovy", "dart", "scala", "c++", "assembly", "prolog")
+    rand_word = choice(WORDS)
 
-output(get_outcome(rand_word, user_word))
+    regex = "[', \\[\\]]+"
+
+    word_hint_list = create_list_of(len(rand_word), "-")
+    word_hint_str = remove_all(word_hint_list.__str__(), regex)
+
+    for num_of_tries in range(0, 10):
+        output(f"\n{word_hint_str}")
+
+        input_letter = input_("Input a letter: ")
+
+        opened = open_letter(rand_word, word_hint_list, input_letter)
+        if not opened:
+            output("No such letter in the word")
+            continue
+
+        word_hint_str = remove_all(word_hint_list.__str__(), regex)
+        if rand_word == word_hint_str:
+            output("\nYou survived!")
+            return
+
+    output("\nYou are hanged!")
+
+
+start_game()
